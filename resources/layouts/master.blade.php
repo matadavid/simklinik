84014fc5628900470c9025a5a67b2fbc8805af6b
AndrianusTinas <Andrianus_tinas.siskom@student.untan.ac.id>
2017-03-14 00:49:04 +0700

    tambah folder layouts serta file masternya

---
+++ resources/layouts/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+  </head>
+  <body>
+    <div class="">
+        <header>
+
+        </header>
+        <hr>
+        @yield('section')
+        <br>
+        <footer>
+          
+        </footer>
+    </div>
+  </body>
+
+</html>
